ef3f974b9d9315c9431b7d8b1c018d233f473780
Laigha Young <laighayoung@Laighas-MacBook-Pro.local>
2024-10-02 19:48:20 -0400

    technicalfinish

--- Block07/Workshop/code.js
+++ Block07/Workshop/code.js
@@ -1,18 +1,19 @@
 /**
- * 1)Declare string variable and assign it to the value: You have received this message because you have been chosen to open an important vault. Here is the secret combination:"
- * 2)Declare + assign 3 varable - 1 for each # in the combination
+ * 1)Declare a string variable and assign it to the value:
+ * "You have received this message because you have been chosen to open an
+ * important vault. Here is the secret combination:"
+ * 2)Declare and assign 3 varables: 1 for each number in the combination
  * 3)Code1 will be using addition
- * 4)Code2 will use subtraction
- * 5)Code3 will use multiplication
+ * 4)Code2 will using subtraction
+ * 5)Code3 will using multiplication
  * 6) Display strings and codes to user (2 options)
- * 
  */
 
+const userMessage =
+  "You have received this message because you have been chosen to open an important vault. Here is the secret combination:";
 
-const userMessage = "You have received this message because you have been chosen to open an important vault. Here is the secret combination:"
+let code1 = 1 + 1;
+let code2 = 10 - 5;
+let code3 = 7 * 3;
 
-let code1 = 1 + 1
-let code2 = 10 - 5
-let code3 = 7 * 3
-
-alert (userMessage + "\n" + "code1" + "code2" + "code3")
+alert(userMessage + "\n" + code1 + " " + code2 + " " + code3);
