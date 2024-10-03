ea87b7ebf5151862d8a45fd980828d87581dad88
Laigha Young <laighayoung@Laighas-MacBook-Pro.local>
2024-10-02 20:40:34 -0400

    editing

--- Block07/Workshop/code.js
+++ Block07/Workshop/code.js
@@ -3,17 +3,17 @@
  * "You have received this message because you have been chosen to open an
  * important vault. Here is the secret combination:"
  * 2)Declare and assign 3 varables: 1 for each number in the combination
- * 3)Code1 will be using addition
- * 4)Code2 will using subtraction
- * 5)Code3 will using multiplication
+ * 3)Code1 will be using subtraction
+ * 4)Code2 will using multiplication
+ * 5)Code3 will using addition
  * 6) Display strings and codes to user (2 options)
  */
 
 const userMessage =
   "You have received this message because you have been chosen to open an important vault. Here is the secret combination:";
 
-let code1 = 1 + 1;
-let code2 = 10 - 5;
-let code3 = 7 * 3;
+let code1 = 11 - 1;
+let code2 = 10 * 4;
+let code3 = 36 + 3;
 
 alert(userMessage + "\n" + code1 + " " + code2 + " " + code3);
