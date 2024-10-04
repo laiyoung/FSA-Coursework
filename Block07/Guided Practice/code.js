347f251bef406bb18ba1d39bfb72ee2e6685369f
Laigha Young <laighayoung@Laighas-MacBook-Pro.local>
2024-10-03 21:01:53 -0400

    comments

--- Block07/Guided Practice/code.js
+++ Block07/Guided Practice/code.js
@@ -3,15 +3,15 @@
  * 2)Find the product referred to as "toothpaste"
  * 3)Locate a sink hooked up to running water that is preferably sanitized
  * 4)Turn the toothbrush so that the fluffy bristles are in an upward position
- * 5)Turn or push the extending handles on the sink, so that water is flowing from 
+ * 5)Turn or push the extending handles on the sink, so that water is flowing from
  * the curved opening, referred to as a "faucet", into the basin beneath, referred
  * to as a "bowl"
  * 6)Put the toothbrush under the running water, again  with bristles facing upward
  * 7)Remove toothbrush once wet and turn off the running water
- * 8)With the toothbrush bristles up, unscrew the cap of the toothpaste and 
+ * 8)With the toothbrush bristles up, unscrew the cap of the toothpaste and
  * apply the formula to the toothbrush bristles
  * 9)Place the toothbrush bristles, with the toothpaste applied, onto your teeth
- * 10) Move the toothbrush back and forth across your teeth and gums to 
+ * 10) Move the toothbrush back and forth across your teeth and gums to
  * form a frothing lather
  * 11)Make sure the lather reaches all your teeth and your toungue
  * 12)Remove brush from your mouth and spit the lather into the bowl of the sink.
@@ -21,24 +21,26 @@
  * 15)Turn faucet off and dry your toothbrush with a towel or papertowel
  */
 
-
 // Declartion of a variable for 50 cows:
 let numCows = 50;
 
 // Create a string that says "Hello World":
-const x = "Hello World"; 
+const x = "Hello World";
 
 /** Display Hello World in the console log (open a folder terminal + use 
 node [code.js]. You should get Hello World, since defined "x"): 
-*/ 
-console.log (x)
+*/
+console.log(x);
 
-// Concatenate the strings "Hello, " and "Disney Land!" 
-console.log ("Hello," + " " + "Disney Land!")
+// Concatenate the strings "Hello, " and "Disney Land!"
+console.log("Hello," + " " + "Disney Land!");
 
 // Creat a variable with an addition operator, then use console.log to execute:
-let num = 2 + 4
-console.log (num)
+let num = 2 + 4;
+console.log(num);
 
-// Concatenate three strings: Hello + space + World
+/** Concatenate three strings: Hello + space + World
+ * (can't be active for console.logs to work, so it's currently a comment)
+ */
+
 // alert ("Hello" + " " + "World!")
