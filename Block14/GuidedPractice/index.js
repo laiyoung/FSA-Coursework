4e40cc553e6416b1a342d803bee4d686c2a8e0c5
Laigha Young <laighayoung@Laighas-MacBook-Pro.local>
2024-10-29 16:31:43 -0400

    guidedpractice

--- Block14/GuidedPractice/index.js
+++ Block14/GuidedPractice/index.js
@@ -3,12 +3,11 @@ myInstruments.push("tuba");
 myInstruments.push("bassoon");
 myInstruments.splice(2, 1);
 myInstruments.unshift("saxophone");
-console.log (myInstruments)
+console.log(myInstruments);
 
-/**
- * @param {string[]} instruments an array of instruments
- * @returns {string} the first instrument
- */
-function getFirstInstrument(instruments) {
-        return instruments[0];
-      }
+console.log (myInstruments[0]);
+
+console.log (myInstruments[7-1]);
+
+console.log ([myInstruments[0], myInstruments[7-1]])
+
