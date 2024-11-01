4a4c1aaf50402a4f8aa442e3d0f4142e919f2d6a
Laigha Young <laighayoung@Laighas-MacBook-Pro.local>
2024-11-01 14:31:09 -0400

    editing

--- Block14/Workshop/index.js
+++ Block14/Workshop/index.js
@@ -37,7 +37,7 @@ console.log(`The odd numbers you gave are ${getOdds(numbers)}.`);
  * @returns {number} the length of the array
  */
 function getLength(numbers) {
-  // Get numbers with an length array method
+  // Get numbers with a length array method
   // Return length of array
   return numbers.length;
 }
