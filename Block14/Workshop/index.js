2ec0a5a658e2116a83d5883c9db690ef24fd5184
Laigha Young <laighayoung@Laighas-MacBook-Pro.local>
2024-11-01 14:27:36 -0400

    addingEven+OddFunctions

--- Block14/Workshop/index.js
+++ Block14/Workshop/index.js
@@ -37,9 +37,8 @@ console.log(`The odd numbers you gave are ${getOdds(numbers)}.`);
  * @returns {number} the length of the array
  */
 function getLength(numbers) {
-  // Get numbers
-  // return length of array
-
+  // Get numbers with an length array method
+  // Return length of array
   return numbers.length;
 }
 
@@ -48,14 +47,13 @@ function getLength(numbers) {
  * @returns {number} the sum of the numbers
  */
 function getSum(numbers) {
-  // Get numbers
-  // return sum
-
+  // Get array numbers with a while loop
   let sum = 0;
   let i = -1;
   while (++i < numbers.length) {
     sum += numbers[i];
   }
+  // Return sum
   return sum;
 }
 
@@ -64,12 +62,12 @@ function getSum(numbers) {
  * @returns {number} the mean of the numbers
  */
 function getMean(numbers) {
-  //Find the sum
+  //Find the sum with a for loop
   let sum = 0;
   for (let i in numbers) {
     sum += numbers[i];
   }
-  //Get the length of the array
+  //Get the length of the array and define it as "numbersCnt"
   let numbersCnt = numbers.length;
   //Return the average/mean
   return sum / numbersCnt;
@@ -80,7 +78,11 @@ function getMean(numbers) {
  * @returns {number} the smallest of the numbers
  */
 function getMin(numbers) {
+  /** Use a sort method to sort numbers smallest to largest,
+   * with a callback function
+   */
   let smallest = numbers.sort((a, b) => a - b);
+  // Return the smallest number, which is index 0
   return smallest[0];
 }
 
@@ -89,7 +91,12 @@ function getMin(numbers) {
  * @returns {number} the largest of the numbers
  */
 function getMax(numbers) {
+  /** Use a sort method to sort numbers largest to
+   * smallest, with a callback function
+   */
+
   let largest = numbers.sort((a, b) => b - a);
+  // Return the largest number, which is index 0
   return largest[0];
 }
 
@@ -98,6 +105,10 @@ function getMax(numbers) {
  * @returns {number} the range of the numbers (max - min)
  */
 function getRange(numbers) {
+  /** Use the smallest and largest returns, defined as "getMax" and "getMin",
+   * from the previously executed functions to return the range:
+   * "getMax" - "getMin" returns "getRange"
+   */
   return getMax(numbers) - getMin(numbers);
 }
 
@@ -106,8 +117,23 @@ function getRange(numbers) {
  * @returns {number[]} the even numbers in the array
  */
 function getEvens(numbers) {
-  let n = numbers 
-  return n % 2 == 0;
+  /** Invoke a forEach array method on the array defined as "numbers",
+   * with a callback function that adds an element to a new array "getEvens",
+   * when an element is even (even is demarcated using a modulo operator)
+   */
+  const getEvens = [];
+  numbers.forEach((num) => {
+    if (num % 2 === 0) {
+      getEvens.push(num);
+    }
+  });
+
+  // Return the new array "getEvens"
+  return getEvens;
+
+  /** Alternate function:
+   * const getEvens = numbers.filter((num) => num % 2 === 0);
+   */
 }
 
 /**
@@ -115,6 +141,21 @@ function getEvens(numbers) {
  * @returns {number[]} the odd numbers in the array
  */
 function getOdds(numbers) {
-  let n = numbers 
-  return n % 2 > 0;
+  /** Invoke a forEach array method on the array defined as "numbers",
+   * with a callback function that adds an element to a new array "getOdds",
+   * (odd is demarcated using a modulo operator)
+   */
+  const getOdds = [];
+  numbers.forEach((num) => {
+    if (num % 2 > 0) {
+      getOdds.push(num);
+    }
+  });
+
+  // Return the new array "getOdds"
+  return getOdds;
+
+  /** Alternate function:
+   * const getOdds = numbers.filter((num) => num % 2 > 0);
+   */
 }
