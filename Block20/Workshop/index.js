1444577406691b65dd92ef5fd347fe0a87a0272c
Laigha Young <laighayoung@Laighas-MacBook-Pro.local>
2024-11-30 14:02:48 -0500

    technicalfinish

--- Block20/Workshop/index.js
+++ Block20/Workshop/index.js
@@ -4,46 +4,75 @@ const state = {
   targetEven: [],
   targetOdd: [],
 };
-const form = document.querySelector("form");
+const addNumsForm = document.querySelector("form");
 const sortOneButton = document.getElementById("sortOne");
 const sortAllButton = document.getElementById("sortAll");
 
-
 // === Event Listeners + Associated Functions ===
 
-//Event Listeners:
-form.addEventListener("submit", function(event) {
-event.preventDefault();
-/** Add Numbers Function */
+//Adding Numbers to Form:
+addNumsForm.addEventListener("submit", function (event) {
+  event.preventDefault();
+  /** Add Numbers Function */
+  const number = addNumsForm.elements.number.value;
+  if (number != "") {
+    state.start.push(number);
+    console.log(state);
+  }
 
-
+  function renderStartingNumbers() {
+    const numberBankDisplay = document.querySelector("#numberBank output");
+    numberBankDisplay.innerHTML = state.start;
+  }
+  renderStartingNumbers()
 });
 
+//Sort By 1:
+sortOneButton.addEventListener("click", function (event) {
+  event.preventDefault();
+  /** Sort By 1 Function */
+  const number = state.start.shift();
+  sortNumber(number);
+  render(); //Necessary to trigger sort renders
+});
 
+//Sort By All:
+sortAllButton.addEventListener("click", function (event) {
+  event.preventDefault();
+  /** Sort All Function */
+  while (state.start.length > 0) {
+    const number = state.start.shift();
+    sortNumber(number);
+  }
+  render(); //Necessary to trigger sort renders
+});
 
-/** Sort By 1 Function */
-
-/** Sort All Function */
-
-// === Renders ===
-
-/** Rendering Starting Bank */
-function renderStartingNumbers() {
-
+// === Even/Odd Function for Nesting in Sort Functions ===
+function sortNumber(number) {
+  if (number % 2 === 0) {
+    state.targetEven.push(number);
+  } else {
+    state.targetOdd.push(number);
+  }
 }
+
+// === Sorting Renders ===
+
 /** Rendering Even Bank*/
 function renderEvenNumbers() {
-
+  const evenNumberDisplay = document.querySelector("#evens output");
+  evenNumberDisplay.innerHTML = state.targetEven;
 }
 
 /** Rendering Odd Bank */
 function renderOddNumbers() {
-
+  const oddNumberDisplay = document.querySelector("#odds output");
+  oddNumberDisplay.innerHTML = state.targetOdd;
 }
 
-//Rendering all bank functions:
+//Rendering sorting functions:
 function render() {
-  renderStartingNumbers();
+  // renderStartingNumbers();
   renderEvenNumbers();
   renderOddNumbers();
 }
@@ -51,13 +80,18 @@ function render() {
 // Initial render
 render();
 
+/** BTW: The variable number has functional scope, so its reference is changing in
+ * in each function. In addNumsForm, it's referring to the user input numbers. In
+ * the sort functions, it's referring to the entered numbers that are now sitting
+ * in the state.start array. But since the scope is functional, it has to be
+ * defined in each of those functions.
+ */
 
-// Moving Numbers:
-// function moveNumber(number) {
-//   if (number % 2 === 0) {
-//     state.targetEven.push(state.start.pop());
-//   } else {
-//     state.targetOdd.push(state.start.pop());
-//   }
-//   render();
-// }
+/** Originally, I had all the render functions all together, including the 
+ * Starting Number render, but that meant the number bank wouldn't display; 
+ * even though the numbers would pop up when they were moved to the odd or
+ * even bank. I had to move the Starting Number render to INSIDE the addNumsForm
+ * event listener for the numbers to pop up. This also meant removing the Starting
+ * Number render from the bottom render function. 
+ *  
+*/
