28a6a27cf462aeb9ef82c5e6b7f0e63888d15273
Laigha Young <laighayoung@Laighas-MacBook-Pro.local>
2024-12-04 23:20:27 -0500

    fixingURL

--- Block21/Workshop/index.js
+++ Block21/Workshop/index.js
@@ -1,5 +1,5 @@
 const COHORT = "2409-GHP-ET-WEB-PT";
-const API_URL = `https://fsa-crud-2aa9294fe819.herokuapp.com/api/${COHORT}/events`;
+const API_URL = `https://fsa-crud-2aa9294fe819.herokuapp.com/api/${COHORT}`;
 
 // === State ===
 
