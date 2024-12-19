7e22fd062b0eb308684b1affca613fe80594f828
laiyoung <l.d.young@outlook.com>
2024-12-18 19:29:16 -0500

    technicalfinish

--- Block24/Workshop/puppy-pals/src/App.jsx
+++ Block24/Workshop/puppy-pals/src/App.jsx
@@ -1,13 +1,21 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
-import "./App.css";
+import "./app.css";
 import { puppyList } from "./data.js";
+
 
 function App() {
   const [count, setCount] = useState(0);
   console.log("puppyList: ", puppyList);
   const [puppies, setPuppies] = useState(puppyList);
+  const [featPupId, setFeatPupId] = useState(null);
+
+  // function handleClick() {
+  //   // {()=>{console.log("puppy id: ", puppy.id)}}
+  //   // some logic here
+  // }
+  const featuredPup = puppies.find((pup) => pup.id === featPupId);
   return (
     <>
       <div>
@@ -30,13 +38,32 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+
+      {/* Puppy Code */}
+
       <div className="App">
-      {
-        puppies.map((puppy) => {
-             return <p key={puppy.id}>{puppy.name}</p>;
-           })
-       }
-    </div>
+        {puppies.map((puppy) => {
+          return (
+            <p
+              onClick={() => {
+                setFeatPupId(puppy.id);
+              }}
+              key={puppy.id}
+            >
+              {puppy.name}
+            </p>
+          );
+        })}
+      </div>
+      {featPupId && (
+        <div>
+          <h2>{featuredPup.name}</h2>
+          <ul>
+            <li>Age: {featuredPup.age}</li>
+            <li>Email: {featuredPup.email}</li>
+          </ul>
+        </div>
+      )}
     </>
   );
 }
