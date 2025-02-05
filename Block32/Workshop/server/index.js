e8999c2916a2661c6106b8aad44c8837391e4d8a
laiyoung <l.d.young@outlook.com>
2025-02-05 16:22:27 -0500

    portupdate

--- Block32/Workshop/server/index.js
+++ Block32/Workshop/server/index.js
@@ -1,46 +1,52 @@
-// imports here for express and pg
-const pg = require('pg');
-const express = require('express');
-const client = new pg.Client(process.env.DATABASE_URL || 'postgres://localhost/acme_ice_cream_db');
+// Imports here for express and pg:
+const pg = require("pg");
+const express = require("express");
+const client = new pg.Client(
+  process.env.DATABASE_URL || "postgres://localhost/acme_ice_cream_db"
+);
 const app = express();
-const path = require('path');
+const path = require("path");
 
-
-// static routes here (you only need these for deployment + front end)
+// Static routes here (you only need these for deployment + front end):
 app.use(express.static(path.join(__dirname, "../client/dist")));
 
-// app routes here
-app.get('/api/flavors', async(req, res, next) => {
-        try{
-            const SQL = `
+// All app routes here: 
+
+// Get all flavors
+app.get("/api/flavors", async (req, res, next) => {
+  try {
+    const SQL = `
             SELECT * FROM flavors
             `;
-            const response = await client.query(SQL);
-            res.send(response.rows);
-    
-        }catch(ex){ 
-            next()
-        }
-    
-    })
-    app.get('/api/flavors/:id', async(req, res, next) => {
-        try{
-            const SQL = `
+    const response = await client.query(SQL);
+    res.send(response.rows);
+  } catch (ex) {
+    next();
+  }
+});
+
+// Get 1 flavor by id 
+app.get("/api/flavors/:id", async (req, res, next) => {
+  try {
+    const SQL = `
             SELECT * FROM flavors WHERE id = $1
             `;
-            const response = await client.query(SQL, ([req.params.id]));
-            res.send(response.rows);
-    
-        }catch(ex){ 
-            next()
-        }
-    
-    })
+    const response = await client.query(SQL, [req.params.id]);
+    res.send(response.rows);
+  } catch (ex) {
+    next();
+  }
+});
 
-// create your init function
+//Add a flavor
+
+// Delete a flavor
+// Change a flavor
+
+// Create your init function:
 const init = async () => {
-        await client.connect();
-const SQL = `
+  await client.connect();
+  const SQL = `
 DROP TABLE IF EXISTS flavors;
 CREATE TABLE flavors(
 id SERIAL PRIMARY KEY,
@@ -51,13 +57,14 @@ updated_at TIMESTAMP DEFAULT now()
 );
 
 INSERT INTO flavors(name, is_favorite) VALUES('vanilla', false), ('chocolate', true), ('strawberry', false)
-`
+`;
 
-await client.query(SQL)
+  await client.query(SQL);
 
-console.log('data seeded')
-app.listen(3000,() => console.log(`listening on port 3000`))
+  console.log("data seeded");
 
-}
-// init function invocation
+  const port = process.env.PORT || 3000;
+  app.listen(port, () => console.log(`listening on port ${port}`));
+};
+// Init function invocation:
 init();
