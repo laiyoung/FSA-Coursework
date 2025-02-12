25fe7bebaba375101891fdda4cba9901b6ae9be7
laiyoung <l.d.young@outlook.com>
2025-02-11 22:42:16 -0500

    initialdatabases

--- Block34/Workshop/server/db.js
+++ Block34/Workshop/server/db.js
@@ -1,2 +1,109 @@
-/** Holding the data layer for SQL interactions */ 
+/** Holding the data layer for SQL interactions (Postgres Connection) */
 
+// Imports here for pg:
+const pg = require("pg");
+
+// Adding UUID generation capability:
+const uuid = require("uuid");
+
+// Client set up:
+const client = new pg.Client(
+  process.env.DATABASE_URL || "postgres://localhost/acme_dining_db"
+);
+
+// Creating a customer data function:
+const createCustomer = async (customerName) => {
+  console.log("db initialized");
+  const SQL = `
+        INSERT INTO customers(id, name) VALUES($1, $2)
+        RETURNING *
+        `;
+  const result = await client.query(SQL, [uuid.v4(), customerName]);
+  return result.rows[0];
+};
+
+// Creating a restaurant data function:
+const createRestaurant = async (restaurantName) => {
+  console.log("db initialized");
+  const SQL = `
+              INSERT INTO restaurants(id, name) VALUES($1, $2)
+              RETURNING *
+              `;
+  const result = await client.query(SQL, [uuid.v4(), restaurantName]);
+  return result.rows[0];
+};
+
+// Creating a reservation data function:
+const createReservation = async (
+  customerName,
+  restaurantName,
+  date,
+  partyCount
+) => {
+  const SQL = ` 
+        INSERT INTO reservations(id, date, party_count, restaurant_id, customer_id) VALUES($1, $2, $3,
+         (SELECT id FROM restaurants WHERE name =$4), (SELECT id FROM customers WHERE name =$5))  
+         RETURNING *`;
+  const result = await client.query(SQL, [
+    uuid.v4(),
+    date,
+    partyCount,
+    restaurantName,
+    customerName,
+  ]);
+  return result.rows[0];
+};
+
+// Create your init function:
+const init = async () => {
+  await client.connect();
+
+  const SQL = `
+      DROP TABLE IF EXISTS reservations;
+      DROP TABLE IF EXISTS customers;
+      DROP TABLE IF EXISTS restaurants;
+      
+      CREATE TABLE restaurants(
+      id UUID PRIMARY KEY,
+      name VARCHAR(50) NOT NULL
+      );
+
+      CREATE TABLE customers(
+      id UUID PRIMARY KEY,
+      name VARCHAR(50) NOT NULL
+      );
+      
+      CREATE TABLE reservations(
+      id UUID PRIMARY KEY,
+      date DATE NOT NULL,
+      party_count INTEGER NOT NULL,
+      restaurant_id UUID REFERENCES restaurants(id) NOT NULL,
+      customer_id UUID REFERENCES customers(id) NOT NULL
+      );
+      
+      `;
+
+  await client.query(SQL);
+
+  ["Bob", "Janice", "Jerry"].forEach(async (name) => {
+    await createCustomer(name);
+    console.log("customer created: " + name);
+  });
+
+  ["Nobu", "76", "Chili's"].forEach(async (name) => {
+    await createRestaurant(name);
+    console.log("restaurant created: " + name);
+  });
+
+ reservation =  await createReservation("Bob", "Nobu", "2025-02-14", 2);
+  console.log("reservation created:", reservation);
+  // Requires date format of: yyyy-mm-dd
+};
+
+//Allowing the init function to be exported:
+module.exports = {
+  init,
+  createCustomer,
+  createRestaurant,
+  createReservation,
+};
--- Block34/Workshop/server/index.js
+++ Block34/Workshop/server/index.js
@@ -1,13 +1,22 @@
-/** Holding the Express application and API functions */ 
+/** Holding the Express application and API functions */
 
-// Imports here for express and pg:
-const pg = require("pg");
+// Imports here for express:
 const express = require("express");
-const client = new pg.Client(
-  process.env.DATABASE_URL || "postgres://localhost/acme_dining_db"
-);
 const app = express();
 const path = require("path");
+
+// Importing db file:
+const db = require("./db");
+
+// Init function invocation:
+const init = async () => {
+  await db.init();
+
+  const port = process.env.PORT || 3000;
+  app.listen(port, () => console.log(`listening on port ${port}`));
+};
+
+init();
 
 // Parser for the JSON text:
 app.use(express.json());
@@ -18,6 +27,18 @@ app.use(require("morgan")("dev"));
 app.use(express.static(path.join(__dirname, "../client/dist")));
 
 // All app routes here:
+
+//Add a customer
+app.post("/api/customers", async (req, res, next) => {
+  try {
+    console.log(req.body);
+
+    const result = await db.createCustomer(req.body.name);
+    res.send(result);
+  } catch (ex) {
+    next(ex);
+  }
+});
 
 // Get all employees
 // app.get("/api/employees", async (req, res, next) => {
@@ -40,25 +61,6 @@ app.use(express.static(path.join(__dirname, "../client/dist")));
 //             `;
 //     const response = await client.query(SQL);
 //     res.send(response.rows);
-//   } catch (ex) {
-//     next(ex);
-//   }
-// });
-
-//Add an employee
-// app.post("/api/employees", async (req, res, next) => {
-//   try {
-//     console.log(req.body);
-//     const SQL = `
-//               INSERT INTO employees(name, department_id)
-//               VALUES($1,(SELECT id from departments WHERE name=$2))
-//               RETURNING *
-//               `;
-//     const response = await client.query(SQL, [
-//       req.body.name,
-//       req.body.department,
-//     ]);
-//     res.send(response.rows[0]);
 //   } catch (ex) {
 //     next(ex);
 //   }
@@ -97,44 +99,3 @@ app.use(express.static(path.join(__dirname, "../client/dist")));
 //     next(error);
 //   }
 // });
-
-// Create your init function:
-const init = async () => {
-  await client.connect();
-
-  const SQL = `
-DROP TABLE IF EXISTS employees;
-DROP TABLE IF EXISTS departments;
-
-CREATE TABLE departments(
-id SERIAL PRIMARY KEY,
-name VARCHAR(50)
-);
-
-CREATE TABLE employees(
-id SERIAL PRIMARY KEY,
-name VARCHAR(50),
-created_at TIMESTAMP DEFAULT now(),
-updated_at TIMESTAMP DEFAULT now(),
-department_id INTEGER REFERENCES departments(id) NOT NULL
-);
-
-INSERT INTO departments (name) VALUES ('accounting'), ('creative'), ('information technology'), ('human resources');
-INSERT INTO employees (name, department_id) VALUES ('Donna', (SELECT id from departments WHERE name = 'accounting')),
-('Alistair', (SELECT id from departments WHERE name = 'creative')), ('Tracey', (SELECT id from departments WHERE name = 'information technology')),
-('Henry Russell', (SELECT id from departments WHERE name = 'human resources'))
-
-;
-
-
-`;
-
-  await client.query(SQL);
-
-  console.log("data seeded");
-
-  const port = process.env.PORT || 3000;
-  app.listen(port, () => console.log(`listening on port ${port}`));
-};
-// Init function invocation:
-init();
